bc96f6180a4f8a4e1b3bf6f58d6e4697a4a2eee5
athinacabana <athina.cabana@cardenalspinola.com.ar>
2023-02-13 09:45:53 -0300

    agrego login y register

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ const path = require("path")
 
 const app = express();
 
-const publicPath = path.resolve(__dirname, "./public")
-app.use( express.static(publicPath) );
+//const publicPath = path.resolve(__dirname, "./public")
+
+app.use("/", express.static("public"))
 
 app.listen(3000, () => {
 console.log("servidor corriendo en el puerto 3000")
 });
 
-app.get("/", (req,res) => {
-    res.sendFile(path.resolve(__dirname, "./views/home.html"))
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
 });
